6ae789022559185f8ec1457b5e31cd34282969eb
Alisson Menezes <alisson.copyleft@gmail.com>
2020-06-10 12:26:34 +0200

    Create test_app.py

---
+++ test_app.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+ 
+import os
+import tempfile
+ 
+import pytest
+ 
+from app import app
+ 
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    client = app.test_client()
+ 
+    yield client
+ 
+def test_valid_transaction(client):
+    card = {
+            "status": True,
+            "number":123456,
+            "limit":1000,
+            "transaction":{
+                "amount":500
+            }
+        }
+    rv = client.post("/api/transaction",json=card)    
+    assert  True == rv.get_json().get("approved")    
+    assert  500 == rv.get_json().get("newLimit")
+ 
+def test_above_limit(client):
+    card = {
+            "status": True,
+            "number":123456,
+            "limit":1000,
+            "transaction":{
+                "amount":1500
+            }
+        }
+    rv = client.post("/api/transaction",json=card)    
+    assert  False == rv.get_json().get("approved")
+    assert  "Transaction above the limit" in rv.get_json().get("reason")
+ 
+def test_blocked_card(client):
+    card = {
+            "status": False,
+            "number":123456,
+            "limit":1000,
+            "transaction":{
+                "amount":500
+            }
+        }
+    rv = client.post("/api/transaction",json=card)    
+    assert  False == rv.get_json().get("approved")
+    assert  "Blocked Card" in rv.get_json().get("reason")
